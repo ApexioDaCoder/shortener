cf8250c7486affc39dcad8fa6a1e292d08d83059
ApexioDaCoder <apexiodacoder@gmail.com>
2021-12-31 16:36:20 -0600

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -127,7 +127,7 @@ const HomeView = () => {
           OnURL
         </ExternalLink>
       </Alert>
-      <Spacer flexDirection="column" marginY={1} />
+      <Spacer flexDirection="column" marginY={1}></Spacer>
       <Formik<ShortUrlInput>
         initialValues={initialValues}
         validationSchema={shortUrlInputSchema}
